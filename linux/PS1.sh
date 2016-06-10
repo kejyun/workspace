8186b55b982569119454ef8becbbbe09dcb98e3a
KeJyun <kejyun@gmail.com>
2016-06-10 21:17:50 +0800

    kejyun@KeJyuns-MacBook-Pro ~/KeJyun/Book/workspace (master) $

--- linux/PS1.sh
+++ linux/PS1.sh
@@ -4,3 +4,16 @@ export PS1="\n\[\e[1;37m\]|-- \[\e[1;32m\]\u\[\e[0;39m\]@\[\e[1;36m\]\h\[\e[0;39
 
 # kejyun@KeJyundeMBP:
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+
+# kejyun@KeJyuns-MacBook-Pro ~/KeJyun/Book/workspace (master) $
+function color_my_prompt {
+    local __user_and_host="\[\033[01;32m\]\u@\h"
+    local __cur_location="\[\033[01;34m\]\w"
+    local __git_branch_color="\[\033[31m\]"
+    #local __git_branch="\`ruby -e \"print (%x{git branch 2> /dev/null}.grep(/^\*/).first || '').gsub(/^\* (.+)$/, '(\1) ')\"\`"
+    local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
+    local __prompt_tail="\[\033[35m\]$"
+    local __last_color="\[\033[00m\]"
+    export PS1="$__user_and_host $__cur_location $__git_branch_color$__git_branch$__prompt_tail$__last_color "
+}
+color_my_prompt
